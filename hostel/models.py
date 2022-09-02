f529deaf9d5b06160cfb12d9572a0a632b9196ed
Pranshu1902 <pranshu1902@gmail.com>
2022-09-02 19:07:07 +0530

    added mdoels

--- hostel/models.py
+++ hostel/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class LeaveRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reason = models.CharField(max_length=3000)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    visiting_place = models.CharField(max_length=100)
+
+class Complaint(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    complaint = models.CharField(max_length=3000)
+    expected_time = models.DateTimeField()
+
+class HealthReport(models.Model):
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    reportee = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+
+class RoomCleaning(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.TimeField()
